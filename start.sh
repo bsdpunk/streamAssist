e914975570950f5b64187a4e3982ed26b5d6a6f4
Dusty Carver <bsdpunk@gmail.com>
2020-04-11 02:44:44 -0500

    First

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: start.sh
+# 
+#         USAGE: ./start.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 04/11/2020 01:50
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+screenkey -m
+nm-applet
+streaming $1
+alias mpv="mpv -vo caca"
